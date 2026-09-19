c22a156c3b234cc727e36f628cf5df2db468dcae
Tasneem Mubashshira <tasneemmubashshira@Mac.cable.rcn.com>
2026-09-18 21:07:18 -0500

    Fixing Issues 2

--- src/components/TaskBar.jsx
+++ src/components/TaskBar.jsx
@@ -89,7 +89,7 @@ export default function TaskBar({ task, taskIndex, totalTasks, active = true, on
               fontFamily:'inherit',
             }}
           >
-            ✓ Done
+            ✓ Skip
           </button>
         </div>
 
